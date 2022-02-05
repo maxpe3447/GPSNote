e881f0317de9c8b68aaf7f4ded54a2e44460314f
mpech <mpech@DESKTOP-B6LQQC0>
2022-02-05 14:21:14 +0200

    Add project files.

---
+++ GPSNote/GPSNote/App.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+//using Xamarin.Essentials.Implementation;
+//using Xamarin.Essentials.Interfaces;
+using Prism;
+using Prism.Navigation;
+using Prism.Ioc;
+
+
+namespace GPSNote
+{
+    public partial class App
+    {
+        public App(IPlatformInitializer initializer)
+            :base(initializer)
+        {
+            
+
+            //MainPage = new MainPage();
+           
+        }
+
+        
+        protected override async void OnInitialized()
+        {
+            InitializeComponent();
+            await NavigationService.NavigateAsync("NavigationPage/MainPage");
+        }
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            //containerRegistry.RegisterSingleton<IAppInfo>
+
+            //
+            containerRegistry.RegisterForNavigation<NavigationPage>();
+            containerRegistry.RegisterForNavigation<Views.MainPage>();
+        }
+
+    }
+}
---
+++ GPSNote/GPSNote/ViewModels/MainPageViewModal.cs
@@ -0,0 +1,14 @@
+﻿using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GPSNote.ViewModels
+{
+    class MainPageViewModal : ViewModelBase
+    {
+        public MainPageViewModal(INavigationService navigationService) : base(navigationService)
+        {
+        }
+    }
+}
