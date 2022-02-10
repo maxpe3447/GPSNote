e2f5f1d0c96c12a4194c848b9d6d092b9e38d692
Max <m.pech.pr@gmail.com>
2022-02-11 01:55:54 +0200

    add com.

--- GPSNote/GPSNote/Models/UserModel.cs
+++ GPSNote/GPSNote/Models/UserModel.cs
@@ -10,6 +10,7 @@ namespace GPSNote.Models
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
 
+        public string Name { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
     }
